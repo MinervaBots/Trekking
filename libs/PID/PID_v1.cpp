c91d9a7c0b952531238fb9053428db75fa0197b2
Brubsy <brunavieira@nano.ufrj.br>
2017-10-09 17:51:17 -0300

    Padronizando a biblioteca do PID

--- libs/PID/PID_v1.cpp
+++ libs/PID/PID_v1.cpp
@@ -1,224 +1,270 @@
 /**********************************************************************************************
- * Arduino PID Library - Version 1.1.1
- * by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
- *
- * This Library is licensed under a GPLv3 License
+   Arduino PID Library - Version 1.1.1
+   by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
+
+   This Library is licensed under a GPLv3 License
  **********************************************************************************************/
 
-#if ARDUINO >= 100
-  #include "Arduino.h"
-#else
-  #include "WProgram.h"
-#endif
+#include "Arduino.h"
 
-#include <PID_v1.h>
+#include "PID_v1.h"
 
 /*Constructor (...)*********************************************************
- *    The parameters specified here are those for for which we can't set up
- *    reliable defaults, so we need to have the user set them.
+      The parameters specified here are those for for which we can't set up
+      reliable defaults, so we need to have the user set them.
  ***************************************************************************/
-PID::PID(double* Input, double* Output, double* Setpoint,
-        double Kp, double Ki, double Kd, int POn, int ControllerDirection)
+PID::PID(float* input, float* output, float* setpoint,
+         float Kp, float Ki, float Kd, int POn, int controllerDirection)
 {
-    myOutput = Output;
-    myInput = Input;
-    mySetpoint = Setpoint;
-    inAuto = false;
+  output_ = output;
+  input_ = input;
+  setpoint_ = setpoint;
+  inAuto = false;
 
-    PID::SetOutputLimits(0, 255);				//default output limit corresponds to
-												//the arduino pwm limits
+  PID::setOutputLimits(0, 255);				//default output limit corresponds to
+  //the arduino pwm limits
 
-    SampleTime = 100;							//default Controller Sample Time is 0.1 seconds
+  sampleTime = 100;							//default Controller Sample Time is 0.1 seconds
 
-    PID::SetControllerDirection(ControllerDirection);
-    PID::SetTunings(Kp, Ki, Kd, POn);
+  PID::setControllerDirection(controllerDirection);
+  PID::setTunings(Kp, Ki, Kd, POn);
 
-    lastTime = millis()-SampleTime;
+  lastTime = millis() - sampleTime;
 }
 
 /*Constructor (...)*********************************************************
- *    To allow backwards compatability for v1.1, or for people that just want
- *    to use Proportional on Error without explicitly saying so
+      To allow backwards compatability for v1.1, or for people that just want
+      to use Proportional on Error without explicitly saying so
  ***************************************************************************/
 
-PID::PID(double* Input, double* Output, double* Setpoint,
-        double Kp, double Ki, double Kd, int ControllerDirection)
-    :PID::PID(Input, Output, Setpoint, Kp, Ki, Kd, P_ON_E, ControllerDirection)
+PID::PID(float* input, float* output, float* setpoint,
+         float Kp, float Ki, float Kd, int controllerDirection)
+  : PID::PID(input, output, setpoint, Kp, Ki, Kd, P_ON_E, controllerDirection)
 {
 
 }
 
 
 /* Compute() **********************************************************************
- *     This, as they say, is where the magic happens.  this function should be called
- *   every time "void loop()" executes.  the function will decide for itself whether a new
- *   pid Output needs to be computed.  returns true when the output is computed,
- *   false when nothing has been done.
+       This, as they say, is where the magic happens.  this function should be called
+     every time "void loop()" executes.  the function will decide for itself whether a new
+     pid Output needs to be computed.  returns true when the output is computed,
+     false when nothing has been done.
  **********************************************************************************/
-bool PID::Compute()
+bool PID::compute()
 {
-   if(!inAuto) return false;
-   unsigned long now = millis();
-   unsigned long timeChange = (now - lastTime);
-   if(timeChange>=SampleTime)
-   {
-      /*Compute all the working error variables*/
-      double input = *myInput;
-      double error = *mySetpoint - input;
-      double dInput = (input - lastInput);
-      outputSum+= (ki * error);
+  if (!inAuto)
+    return false;
 
-      /*Add Proportional on Measurement, if P_ON_M is specified*/
-      if(!pOnE) outputSum-= kp * dInput;
+  unsigned long now = millis();
+  unsigned long timeChange = (now - lastTime);
 
-      if(outputSum > outMax) outputSum= outMax;
-      else if(outputSum < outMin) outputSum= outMin;
+  if (timeChange >= sampleTime)
+  {
+    /*Compute all the working error variables*/
+    float input = *input_;
+    float error = *setpoint_ - input;
+    float dInput = (input - lastInput);
+    outputSum += (ki * error);
 
-      /*Add Proportional on Error, if P_ON_E is specified*/
-	   double output;
-      if(pOnE) output = kp * error;
-      else output = 0;
+    /*Add Proportional on Measurement, if P_ON_M is specified*/
+    if (!pOnE)
+      outputSum -= kp * dInput;
 
-      /*Compute Rest of PID Output*/
-      output += outputSum - kd * dInput;
+    if (outputSum > outMax)
+      outputSum = outMax;
 
-	    if(output > outMax) output = outMax;
-      else if(output < outMin) output = outMin;
-	    *myOutput = output;
+    else if (outputSum < outMin)
+      outputSum = outMin;
 
-      /*Remember some variables for next time*/
-      lastInput = input;
-      lastTime = now;
-	    return true;
-   }
-   else return false;
+    /*Add Proportional on Error, if P_ON_E is specified*/
+    float output;
+
+    if (pOnE)
+      output = kp * error;
+
+    else
+      output = 0;
+
+    /*Compute Rest of PID Output*/
+    output += outputSum - kd * dInput;
+
+    if (output > outMax)
+      output = outMax;
+
+    else if (output < outMin)
+      output = outMin;
+      
+    *output_ = output;
+
+    /*Remember some variables for next time*/
+    lastInput = input;
+    lastTime = now;
+    return true;
+  }
+  else return false;
 }
 
 /* SetTunings(...)*************************************************************
- * This function allows the controller's dynamic performance to be adjusted.
- * it's called automatically from the constructor, but tunings can also
- * be adjusted on the fly during normal operation
+   This function allows the controller's dynamic performance to be adjusted.
+   it's called automatically from the constructor, but tunings can also
+   be adjusted on the fly during normal operation
  ******************************************************************************/
-void PID::SetTunings(double Kp, double Ki, double Kd, int POn)
+void PID::setTunings(float Kp, float Ki, float Kd, int POn)
 {
-   if (Kp<0 || Ki<0 || Kd<0) return;
+  if (Kp < 0 || Ki < 0 || Kd < 0)
+    return;
 
-   pOn = POn;
-   pOnE = POn == P_ON_E;
+  pOn = POn;
+  pOnE = POn == P_ON_E;
 
-   dispKp = Kp; dispKi = Ki; dispKd = Kd;
+  dispKp = Kp;
+  dispKi = Ki;
+  dispKd = Kd;
 
-   double SampleTimeInSec = ((double)SampleTime)/1000;
-   kp = Kp;
-   ki = Ki * SampleTimeInSec;
-   kd = Kd / SampleTimeInSec;
+  float SampleTimeInSec = ((float)sampleTime) / 1000;
+  kp = Kp;
+  ki = Ki * SampleTimeInSec;
+  kd = Kd / SampleTimeInSec;
 
-  if(controllerDirection ==REVERSE)
-   {
-      kp = (0 - kp);
-      ki = (0 - ki);
-      kd = (0 - kd);
-   }
+  if (controllerDirection == REVERSE)
+  {
+    kp = (0 - kp);
+    ki = (0 - ki);
+    kd = (0 - kd);
+  }
 }
 
 /* SetTunings(...)*************************************************************
- * Set Tunings using the last-rembered POn setting
+   Set Tunings using the last-rembered POn setting
  ******************************************************************************/
-void PID::SetTunings(double Kp, double Ki, double Kd){
-    SetTunings(Kp, Ki, Kd, pOn); 
+void PID::setTunings(float Kp, float Ki, float Kd)
+{
+  setTunings(Kp, Ki, Kd, pOn);
 }
 
-/* SetSampleTime(...) *********************************************************
- * sets the period, in Milliseconds, at which the calculation is performed
+/* setSampleTime(...) *********************************************************
+   sets the period, in Milliseconds, at which the calculation is performed
  ******************************************************************************/
-void PID::SetSampleTime(int NewSampleTime)
+void PID::setSampleTime(int newSampleTime)
 {
-   if (NewSampleTime > 0)
-   {
-      double ratio  = (double)NewSampleTime
-                      / (double)SampleTime;
-      ki *= ratio;
-      kd /= ratio;
-      SampleTime = (unsigned long)NewSampleTime;
-   }
+  if (newSampleTime > 0)
+  {
+    float ratio  = (float) newSampleTime
+                   / (float) sampleTime;
+    ki *= ratio;
+    kd /= ratio;
+    sampleTime = (unsigned long) newSampleTime;
+  }
 }
 
 /* SetOutputLimits(...)****************************************************
- *     This function will be used far more often than SetInputLimits.  while
- *  the input to the controller will generally be in the 0-1023 range (which is
- *  the default already,)  the output will be a little different.  maybe they'll
- *  be doing a time window and will need 0-8000 or something.  or maybe they'll
- *  want to clamp it from 0-125.  who knows.  at any rate, that can all be done
- *  here.
+       This function will be used far more often than SetInputLimits.  while
+    the input to the controller will generally be in the 0-1023 range (which is
+    the default already,)  the output will be a little different.  maybe they'll
+    be doing a time window and will need 0-8000 or something.  or maybe they'll
+    want to clamp it from 0-125.  who knows.  at any rate, that can all be done
+    here.
  **************************************************************************/
-void PID::SetOutputLimits(double Min, double Max)
+void PID::setOutputLimits(float Min, float Max)
 {
-   if(Min >= Max) return;
-   outMin = Min;
-   outMax = Max;
+  if (Min >= Max)
+    return;
 
-   if(inAuto)
-   {
-	   if(*myOutput > outMax) *myOutput = outMax;
-	   else if(*myOutput < outMin) *myOutput = outMin;
+  outMin = Min;
+  outMax = Max;
 
-	   if(outputSum > outMax) outputSum= outMax;
-	   else if(outputSum < outMin) outputSum= outMin;
-   }
+  if (inAuto)
+  {
+    if (*output_ > outMax)
+      *output_ = outMax;
+
+    else if (*output_ < outMin)
+      *output_ = outMin;
+
+    if (outputSum > outMax)
+      outputSum = outMax;
+
+    else if (outputSum < outMin)
+      outputSum = outMin;
+  }
 }
 
 /* SetMode(...)****************************************************************
- * Allows the controller Mode to be set to manual (0) or Automatic (non-zero)
- * when the transition from manual to auto occurs, the controller is
- * automatically initialized
+   Allows the controller Mode to be set to manual (0) or Automatic (non-zero)
+   when the transition from manual to auto occurs, the controller is
+   automatically initialized
  ******************************************************************************/
-void PID::SetMode(int Mode)
+void PID::setMode(int Mode)
 {
-    bool newAuto = (Mode == AUTOMATIC);
-    if(newAuto && !inAuto)
-    {  /*we just went from manual to auto*/
-        PID::Initialize();
-    }
-    inAuto = newAuto;
+  bool newAuto = (Mode == AUTOMATIC);
+  if (newAuto && !inAuto)
+  { /*we just went from manual to auto*/
+    PID::initialize();
+  }
+  inAuto = newAuto;
 }
 
 /* Initialize()****************************************************************
- *	does all the things that need to happen to ensure a bumpless transfer
- *  from manual to automatic mode.
+ 	does all the things that need to happen to ensure a bumpless transfer
+    from manual to automatic mode.
  ******************************************************************************/
-void PID::Initialize()
+void PID::initialize()
 {
-   outputSum = *myOutput;
-   lastInput = *myInput;
-   if(outputSum > outMax) outputSum = outMax;
-   else if(outputSum < outMin) outputSum = outMin;
+  outputSum = *output_;
+  lastInput = *input_;
+
+  if (outputSum > outMax)
+    outputSum = outMax;
+
+  else if (outputSum < outMin)
+    outputSum = outMin;
 }
 
 /* SetControllerDirection(...)*************************************************
- * The PID will either be connected to a DIRECT acting process (+Output leads
- * to +Input) or a REVERSE acting process(+Output leads to -Input.)  we need to
- * know which one, because otherwise we may increase the output when we should
- * be decreasing.  This is called from the constructor.
+   The PID will either be connected to a DIRECT acting process (+Output leads
+   to +Input) or a REVERSE acting process(+Output leads to -Input.)  we need to
+   know which one, because otherwise we may increase the output when we should
+   be decreasing.  This is called from the constructor.
  ******************************************************************************/
-void PID::SetControllerDirection(int Direction)
+void PID::setControllerDirection(int direction)
 {
-   if(inAuto && Direction !=controllerDirection)
-   {
-	    kp = (0 - kp);
-      ki = (0 - ki);
-      kd = (0 - kd);
-   }
-   controllerDirection = Direction;
+  if (inAuto && direction != controllerDirection)
+  {
+    kp = (0 - kp);
+    ki = (0 - ki);
+    kd = (0 - kd);
+  }
+  
+  controllerDirection = direction;
 }
 
 /* Status Funcions*************************************************************
- * Just because you set the Kp=-1 doesn't mean it actually happened.  these
- * functions query the internal state of the PID.  they're here for display
- * purposes.  this are the functions the PID Front-end uses for example
+   Just because you set the Kp=-1 doesn't mean it actually happened.  these
+   functions query the internal state of the PID.  they're here for display
+   purposes.  this are the functions the PID Front-end uses for example
  ******************************************************************************/
-double PID::GetKp(){ return  dispKp; }
-double PID::GetKi(){ return  dispKi;}
-double PID::GetKd(){ return  dispKd;}
-int PID::GetMode(){ return  inAuto ? AUTOMATIC : MANUAL;}
-int PID::GetDirection(){ return controllerDirection;}
+float PID::getKp()
+{
+  return dispKp;
+}
 
+float PID::getKi()
+{
+  return dispKi;
+}
+
+float PID::getKd()
+{
+  return dispKd;
+}
+
+int PID::getMode()
+{
+  return inAuto ? AUTOMATIC : MANUAL;
+}
+
+int PID::getDirection()
+{
+  return controllerDirection;
+}
--- libs/PID/PID_v1.h
+++ libs/PID/PID_v1.h
@@ -1,11 +1,10 @@
 #ifndef PID_v1_h
 #define PID_v1_h
+
 #define LIBRARY_VERSION	1.1.1
 
 class PID
 {
-
-
   public:
 
   //Constants used in some of the functions below
@@ -17,74 +16,74 @@ class PID
   #define P_ON_E 1
 
   //commonly used functions **************************************************************************
-    PID(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
-        double, double, double, int, int);//   Setpoint.  Initial tuning parameters are also set here.
+    PID(float*, float*, float*,        // * constructor.  links the PID to the Input, Output, and 
+        float, float, float, int, int);//   Setpoint.  Initial tuning parameters are also set here.
                                           //   (overload for specifying proportional mode)
 
-    PID(double*, double*, double*,        // * constructor.  links the PID to the Input, Output, and 
-        double, double, double, int);     //   Setpoint.  Initial tuning parameters are also set here
+    PID(float*, float*, float*,        // * constructor.  links the PID to the Input, Output, and 
+        float, float, float, int);     //   Setpoint.  Initial tuning parameters are also set here
 	
-    void SetMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
+    void setMode(int Mode);               // * sets PID to either Manual (0) or Auto (non-0)
 
-    bool Compute();                       // * performs the PID calculation.  it should be
+    bool compute();                       // * performs the PID calculation.  It should be
                                           //   called every time loop() cycles. ON/OFF and
                                           //   calculation frequency can be set using SetMode
                                           //   SetSampleTime respectively
 
-    void SetOutputLimits(double, double); // * clamps the output to a specific range. 0-255 by default, but
+    void setOutputLimits(float, float); // * clamps the output to a specific range. 0-255 by default, but
 										                      //   it's likely the user will want to change this depending on
 										                      //   the application
 	
 
 
   //available but not commonly used functions ********************************************************
-    void SetTunings(double, double,       // * While most users will set the tunings once in the 
-                    double);         	    //   constructor, this function gives the user the option
+    void setTunings(float, float,       // * While most users will set the tunings once in the 
+                    float);         	    //   constructor, this function gives the user the option
                                           //   of changing tunings during runtime for Adaptive control
-    void SetTunings(double, double,       // * overload for specifying proportional mode
-                    double, int);         	  
+    void setTunings(float, float,       // * overload for specifying proportional mode
+                    float, int);         	  
 
-	void SetControllerDirection(int);	  // * Sets the Direction, or "Action" of the controller. DIRECT
+	  void setControllerDirection(int);	  // * Sets the Direction, or "Action" of the controller. DIRECT
 										  //   means the output will increase when error is positive. REVERSE
 										  //   means the opposite.  it's very unlikely that this will be needed
 										  //   once it is set in the constructor.
-    void SetSampleTime(int);              // * sets the frequency, in Milliseconds, with which 
+    void setSampleTime(int);              // * sets the frequency, in Milliseconds, with which 
                                           //   the PID calculation is performed.  default is 100
 										  
 										  
 										  
-  //Display functions ****************************************************************
-	double GetKp();						  // These functions query the pid for interal values.
-	double GetKi();						  //  they were created mainly for the pid front-end,
-	double GetKd();						  // where it's important to know what is actually 
-	int GetMode();						  //  inside the PID.
-	int GetDirection();					  //
+    //Display functions ****************************************************************
+	  float getKp();						  // These functions query the pid for interal values.
+	  float getKi();						  //  they were created mainly for the pid front-end,
+	  float getKd();						  // where it's important to know what is actually 
+	  int getMode();						  //  inside the PID.
+	  int getDirection();					  //
 
   private:
-	void Initialize();
+	  void initialize();
 	
-	double dispKp;				// * we'll hold on to the tuning parameters in user-entered 
-	double dispKi;				//   format for display purposes
-	double dispKd;				//
+	  float dispKp;				// * we'll hold on to the tuning parameters in user-entered 
+	  float dispKi;				//   format for display purposes
+	  float dispKd;				//
     
-	double kp;                  // * (P)roportional Tuning Parameter
-    double ki;                  // * (I)ntegral Tuning Parameter
-    double kd;                  // * (D)erivative Tuning Parameter
+	  float kp;                  // * (P)roportional Tuning Parameter
+    float ki;                  // * (I)ntegral Tuning Parameter
+    float kd;                  // * (D)erivative Tuning Parameter
 
-	int controllerDirection;
-	int pOn;
+	  int controllerDirection;
+	  int pOn;
 
-    double *myInput;              // * Pointers to the Input, Output, and Setpoint variables
-    double *myOutput;             //   This creates a hard link between the variables and the 
-    double *mySetpoint;           //   PID, freeing the user from having to constantly tell us
-                                  //   what these values are.  with pointers we'll just know.
+    float *input_;              // * Pointers to the Input, Output, and Setpoint variables
+    float *output_;             //   This creates a hard link between the variables and the 
+    float *setpoint_;           //   PID, freeing the user from having to constantly tell us
+                                //   what these values are.  with pointers we'll just know.
 			  
-	unsigned long lastTime;
-	double outputSum, lastInput;
+	  unsigned long lastTime;
+	  float outputSum, lastInput;
 
-	unsigned long SampleTime;
-	double outMin, outMax;
-	bool inAuto, pOnE;
+	  unsigned long sampleTime;
+	  float outMin, outMax;
+	  bool inAuto, pOnE;
 };
-#endif
 
+#endif // PID_v1_h
