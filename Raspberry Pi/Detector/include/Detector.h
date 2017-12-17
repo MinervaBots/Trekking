301042efa3e3880af5223b0c9f66f276441bed84
PauloHMattos <paulo.mattos@poli.ufrj.br>
2017-12-17 16:50:27 +0000

    Correções no CMakeLists da biblioteca de detecção

--- Raspberry Pi/Detector/include/Detector.h
+++ Raspberry Pi/Detector/include/Detector.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <opencv2\opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include "IntVector2.h"
 #include "IntRect.h"
 
