4dfd02af203c7aaac7a7416d1c40610420e9c71c
igor98 <igorcsi@poli.ufrj.br>
2017-12-17 06:44:54 -0200

    Update MPU.ino

--- Arduino/MPU/MPU.ino
+++ Arduino/MPU/MPU.ino
@@ -5,18 +5,23 @@
 #define E 2.7
 #define G 9.8
 
-#define DECAY 1 // Constante de decaimento para função exponecial usada no cálculo de Km.
+#define DECAY 10000 // constante de decaimento para função exponecial usada no cálculo de Km
 
 #define CALIBRATION_BUTTON 10
 
 MPU9250_DMP imu;
 
 float ax, ay, gyro, mx, my;
-float Km; // Quanto mais próximo a 1, maior o peso do magnetômetro. Quanto mais próximo a 0, maior o peso do giroscópio.
-float offsetMag, offsetGyro, offsetAx, offsetAy;
-float gyroHeading; // Ângulo calculado pelo giroscópio.
-float magHeading; // Ângulo calculado pelo magnetômetro.
-int lastRun; // Guarda a última vez que o giroscópio foi calculado para usar o dt na integral.
+float Km; // quanto mais próximo a 1, maior o peso do magnetômetro, quanto mais próximo a 0, maior o peso do giroscópio
+float offsetMag, offsetGyro, offsetAx, offsetAy, lastGyro;
+float gyroHeading; // ângulo calculado pelo giroscópio
+float magHeading; // ângulo calculado pelo magnetômetro
+long lastRun; // guarda a última vez que o giroscópio foi calculado (para usar o dt na integral)
+float minMxReal, maxMxReal, minMyReal, maxMyReal;
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+ return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 union
 {
@@ -33,36 +38,34 @@ union
 
 PacketSerial serial;
 
-void readMPU() 
-{
+void readMPU() {
   imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
   ax = imu.calcAccel(imu.ax);
   ay = imu.calcAccel(imu.ay);
-  gyro = imu.calcAccel(imu.gz) * PI / 180.0;
+  gyro = imu.calcAccel(imu.gz);
   mx = imu.calcAccel(imu.mx);
   my = imu.calcAccel(imu.my);
 }
 
-void reset() // Encontrar offsets.
-{
+void reset() { // encontrar offsets
   offsetMag = 0;
   gyroHeading = 0;
   offsetGyro = 0;
-  
-  for (int i = 0; i < 1000; i++) 
-  {
+  lastGyro = 0;
+  for (int i = 0; i < 1000; i++) {
     readMPU();
     offsetGyro += gyro;
     offsetAx += ax;
     offsetAy += ay;
-    offsetMag += atan2(mx,my);
+    float mxReal = mapFloat(mx, minMxReal, maxMxReal, -1000.0, 1000.0);
+    float myReal = mapFloat(my, minMyReal, maxMyReal, -1000.0, 1000.0);
+    offsetMag += atan2(mxReal,myReal);
   }
-  
   offsetGyro /= 1000.0;
   offsetAx /= 1000.0;
   offsetAy /= 1000.0;
   offsetMag /= 1000.0;
-  lastRun = millis();
+  lastRun = micros();
 }
 
 void receivedPacket(const uint8_t* buffer, size_t size)
@@ -84,23 +87,20 @@ void receivedPacket(const uint8_t* buffer, size_t size)
   }
 }
 
-void setMPU() 
-{
-  // Use setSensors to turn on or off MPU-9250 sensors.
+void setMPU() {
+   // Use setSensors to turn on or off MPU-9250 sensors.
   // Any of the following defines can be combined:
   // INV_XYZ_GYRO, INV_XYZ_ACCEL, INV_XYZ_COMPASS,
   // INV_X_GYRO, INV_Y_GYRO, or INV_Z_GYRO
   // Enable all sensors:
-    imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
 
   // Use setGyroFSR() and setAccelFSR() to configure the
   // gyroscope and accelerometer full scale ranges.
   // Gyro options are +/- 250, 500, 1000, or 2000 dps
-    imu.setGyroFSR(500); // Set gyro to 500 dps
-  
+  imu.setGyroFSR(2000); // Set gyro to 2000 dps
   // Accel options are +/- 2, 4, 8, or 16 g
-    imu.setAccelFSR(4); // Set accel to +/-4g
-  
+  imu.setAccelFSR(4); // Set accel to +/-4g
   // Note: the MPU-9250's magnetometer FSR is set at 
   // +/- 4912 uT (micro-tesla's)
 
@@ -108,59 +108,87 @@ void setMPU()
   // of the accelerometer and gyroscope.
   // Can be any of the following: 188, 98, 42, 20, 10, 5
   // (values are in Hz).
-    imu.setLPF(5); // Set LPF corner frequency to 5Hz
+  imu.setLPF(5); // Set LPF corner frequency to 5Hz
 
   // The sample rate of the accel/gyro can be set using
   // setSampleRate. Acceptable values range from 4Hz to 1kHz
-    imu.setSampleRate(10); // Set sample rate to 10Hz
+  imu.setSampleRate(100); // Set sample rate to 100Hz
 
   // Likewise, the compass (magnetometer) sample rate can be
   // set using the setCompassSampleRate() function.
   // This value can range between: 1-100Hz
-    imu.setCompassSampleRate(10); // Set mag rate to 10Hz
+  imu.setCompassSampleRate(100); // Set mag rate to 100Hz
+  EEPROM.get(0, minMxReal);
+  EEPROM.get(sizeof(float), maxMxReal);
+  EEPROM.get(2*sizeof(float), minMyReal);
+  EEPROM.get(3*sizeof(float), maxMyReal);
 }
 
-void magnetometerCalibration() 
-{
+void magnetometerCalibration() {
   float minMx, maxMx, minMy, maxMy;
   readMPU();
   minMx = imu.calcAccel(imu.mx);
   maxMx = imu.calcAccel(imu.mx);
   minMy = imu.calcAccel(imu.my);
   maxMy = imu.calcAccel(imu.my);
-  
-  while(digitalRead(CALIBRATION_BUTTON))
-  {
+  long start = millis();
+  while(millis() - start < 20000) {
     readMPU();
     minMx = min(minMx, imu.calcAccel(imu.mx));
     maxMx = max(maxMx, imu.calcAccel(imu.mx));
     minMy = min(minMy, imu.calcAccel(imu.my));
     maxMy = max(maxMy, imu.calcAccel(imu.my));
+    Serial.print("minMx = ");
+    Serial.print(minMx*10000000000);
+    Serial.print("     maxMx = ");
+    Serial.print(maxMx*10000000000);
+    Serial.print("     minMy = ");
+    Serial.print(minMy*10000000000);
+    Serial.print("     maxMy = ");
+    Serial.print(maxMy*10000000000);
+    Serial.print("     ");
+    Serial.println(20000 - (millis() - start));
   }
-  
   EEPROM.put(0, minMx);
   EEPROM.put(sizeof(float), maxMx);
   EEPROM.put(2*sizeof(float), minMy);
   EEPROM.put(3*sizeof(float), maxMy);
 }
 
-void setup() 
-{
+void setup() {
   imu.begin();
   setMPU();
+  Serial.begin(9600);
   serial.begin(9600);
   serial.setPacketHandler(&receivedPacket);
   reset();
 }
 
-void loop()
-{
+void loop() {
   serial.update();
   readMPU();
-  magHeading = atan2(mx, my) - offsetMag;
-  gyroHeading += (gyro - offsetGyro) * (millis() - lastRun); // Integrando a velocidade angular para obter ângulo.
-  Km = pow(E, -DECAY * abs((gyro - offsetGyro))); // Função que encontra Km. Quanto maior a velocidade ângular, maior o peso do giroscópio.
-  gyroHeading = Km * magHeading + (1 - Km) * gyroHeading; // Filtro sendo feito e já atualizando o valor de gyroHeading.
-  acceleration.value = sqrt(pow((ax - offsetAx), 2) + pow((ay - offsetAy), 2))  *G;
+  float mxReal = mapFloat(mx, minMxReal, maxMxReal, -1000.0, 1000.0);
+  float myReal = mapFloat(my, minMyReal, maxMyReal, -1000.0, 1000.0);
+  magHeading = atan2(mxReal,myReal) - offsetMag;
+  magHeading *= (180 / PI);
+  gyroHeading -= (lastGyro + (gyro - offsetGyro)) * (micros() - lastRun) / 4000; // integrando a velocidade angular -> ângulo
+  if (gyroHeading > 180.0) {
+    gyroHeading -= 360.0;
+  }
+  if (gyroHeading < -180.0){
+    gyroHeading += 360.0;
+  }
+  if (magHeading > 180.0) {
+    magHeading -= 360.0;
+  }
+  if (magHeading < -180.0){
+    magHeading += 360.0;
+  }
+  Km = pow(E, -DECAY * abs((gyro - offsetGyro))); // função que encontra Km. Quanto maior a velocidade ângular, maior o peso do gyroscópio
+  gyroHeading = Km * magHeading + (1 - Km) * gyroHeading; // filtro sendo feito e já atualizando o valor de gyroHeading
+  acceleration.value = (ax - offsetAx)*G; //sqrt(pow((ax - offsetAx),2) + pow((ay - offsetAy),2))*G;
   heading.value = gyroHeading;
+  lastRun = micros();
+  lastGyro = gyro - offsetGyro;
+  Serial.println(int(gyroHeading));
 }
