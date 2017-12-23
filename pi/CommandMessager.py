f30599519927203731e0acc70e498a24b7775299
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2017-12-23 14:48:44 -0200

    Default handler e comentários

--- pi/CommandMessager.py
+++ pi/CommandMessager.py
@@ -1,16 +1,14 @@
 from threading import Thread
 import PyCmdMessenger
 import queue
-import datetime
 
 class CommandMessager(Thread):
-
-    def __init__(self, arduino, commands, lock):
+    def __init__(self, arduino, commands, defaulHandler):
         super(CommandMessager, self).__init__()
         self._commands = list(commands)
         self._cmdMessenger = PyCmdMessenger.CmdMessenger(arduino, commands)
         self.sendQueue = queue.Queue()
-        self.lock = lock
+        self.defaulHandler = defaulHandler
         self.isRunning = True
 
     def stop(self):
@@ -20,14 +18,9 @@ class CommandMessager(Thread):
         while self.isRunning:
             if not self.sendQueue.empty():
                 cmd = self.sendQueue.get()
-                #print(cmd)
                 self._cmdMessenger.send(cmd[0], *cmd[1])
-            #start = datetime.datetime.now()
             msg = self._cmdMessenger.receive()
             self.handle(msg)
-            #end = datetime.datetime.now()
-            #delta = (end - start).total_seconds() * 1000
-            #print(delta)
 
     def send(self, cmd, *args):
         self.sendQueue.put([cmd, args])
@@ -35,10 +28,10 @@ class CommandMessager(Thread):
     def handle(self, msg):
         if msg is None:
             return
+        
+        args = msg[1:]
         for command in self._commands:
             if command[0] == msg[0]:
-                args = msg[1:]
-                #print(args)
-                #print(datetime.datetime.now())
                 command[2](*args)
                 return
+        defaulHandler(*args)
--- pi/Trekking.py
+++ pi/Trekking.py
@@ -1,6 +1,6 @@
 from utils.FPS import FPS
 from videoStream.VideoStream import VideoStream
-from threading import Thread, Lock
+from threading import Thread
 import cv2
 import time
 import PyCmdMessenger
@@ -15,20 +15,24 @@ frameWidth = 640
 frameHeight = 368
 frameCenterX = frameWidth / 2.0
 
-def relay(msg, recvTime):
-    #print (msg)
-    return None
+def relayToPC(msg, recvTime):
+    # Defalt handler
+    # Caso nenhum handler seja definido usamos esse
+    # Futuramente ele vai simplesmente encaminhar a mensagem para o PC
+    return
+
+def error(msg, recvTime):
+    print ("ERROR: {}".format(msg))
 
 def servoDirection(position, recvTime):
     print(position)
-    
+
 #PyCmdMessage
-lock = Lock()
 arduino = PyCmdMessenger.ArduinoBoard("COM3", baud_rate = 9600)
-commands = [["targetData", "fffff"],
+commands = [["targetData1", "fffff"],
             ["servoDirection", "f", servoDirection],
-            ["relay", "s"]]
-cmdMessenger = CommandMessager.CommandMessager(arduino, commands, lock)
+            ["error", "s", error]]
+cmdMessenger = CommandMessager.CommandMessager(arduino, commands, relayToPC)
 
 #Medida de performance
 fps = FPS()
@@ -52,27 +56,33 @@ def setup():
 
     fps.stop()
     cmdMessenger.stop()
-    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] Mean. FPS: {:.2f}".format(fps.meanFps()))
+    arduino.close()
     cv2.destroyAllWindows()
     vs.stop()
-    arduino.close()
+    
+    print("[INFO] Tempo percorrido: {:.2f}".format(fps.elapsed()))
+    print("[INFO] FPS médio: {:.2f}".format(fps.meanFps()))
         
 def loop():
-    global isRunning, cnt
+    global isRunning
     frame = vs.read()
     if frame is None:
         print(u'Não foi possível recuperar um frame da câmera')
         return None
-        
+
+    # Haar Cascade trabalha em escala de cinza
+    # Então converte a imagem aqui
+    # Futuramente vou implementar um controle de cor
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     objects = cascadeDetector.detectMultiScale(gray, 1.3, 5)
     if len(objects) > 0:
         (x, y, w, h) = objects[0]
         objCenterX = x + w / 2.0
-        direction = (objCenterX - 0) * (1 - -1) / (frameWidth - 0) + -1
-        #print(direction)
-        cmdMessenger.send("targetData", direction, x, y, w, h)
+
+        # Faz uma interpolação para calcular a direção
+        # do centro do objeto relativo ao centro da tela
+        direction = (objCenterX - 0) * (1 - (-1)) / (frameWidth - 0) + (-1)
+        cmdMessenger.send("targetData1", direction, x, y, w, h)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     if showWindows:
