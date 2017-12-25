7b440f09f13b51afdb3bcaaffbdb01afceea4b6f
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2017-12-25 01:49:44 -0200

    Demo da comunicação serial por bluetooth atualizado
    Agora a implementação é independente da biblitoca PyBluez e depende só da PySerial

--- pi/_Demo_Bluetooth.py
+++ pi/_Demo_Bluetooth.py
@@ -1,41 +1,60 @@
-# file: rfcomm-server.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a server application that uses RFCOMM sockets
-#
-# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
+import serial
+from time import sleep
+import sys
+import queue
+from threading import Thread
+from sys import platform as _platform
 
-from bluetooth import *
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
-server_sock.listen(1)
+isRaspberryPi = "linux" in _platform
 
-port = server_sock.getsockname()[1]
+port = "/dev/rfcomm1" if isRaspberryPi else "COM4"
+baud = 9600
+ser = serial.Serial(port, baudrate=baud, timeout=0.1)
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+sendQueue = queue.Queue()
 
-advertise_service( server_sock, "SampleServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
-                   
-print("Waiting for connection on RFCOMM channel %d" % port)
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
+def exitHandler(cmd):
+    ser.close()
+    sys.exit(0)
 
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
-except IOError:
-    pass
 
-print("disconnected")
+handlers = {"exit": exitHandler}
 
-client_sock.close()
-server_sock.close()
-print("all done")
+def sendCommand(*cmd):
+    cmdAsStr = "/".join(cmd)
+    sendQueue.put(cmdAsStr+"\r\n")
+
+    
+def recvLoop():
+    while ser.is_open:
+        val = str(ser.readline().decode().strip('\r\n'))
+        if(val == ""):
+            continue
+        
+        cmd = val.split("/")
+        if(len(cmd) == 0):
+            continue
+        
+        opCode = cmd[0]
+        if(opCode in handlers):
+            handlers[opCode](cmd)
+        else:
+            sendCommand("error", "Handler invalido")
+
+def sendLoop():
+    while ser.is_open:
+        if not sendQueue.empty():
+            cmd = sendQueue.get()
+            ser.write(cmd.encode())
+            
+recvThread = Thread(target = recvLoop, args = ())
+sendThread = Thread(target = sendLoop, args = ())
+
+recvThread.start()
+sendThread.start()
+
+while ser.is_open:
+    s = input("Envie algo")
+    sendCommand(s)
