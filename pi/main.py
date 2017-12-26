d56d1702d1394c329b79ae6cb5aba34e5eb71747
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2017-12-26 17:17:43 -0200

    Inicializa a camera somente quando requisitado
    E também só roda o código de detectção quando requisitado.
    
    Evita gastar processamento tentando reconhecer o cone de muito longe, e economiza bateria

--- pi/main.py
+++ pi/main.py
@@ -14,13 +14,24 @@ from sys import platform as _platform
 
 
 isRunning = False
+isRunningDetection = False
+
 def start(message):
     global isRunning
     isRunning = True
-    
+
 def stop(message):
     global isRunning
     isRunning = False
+
+def startDetection(message = None):
+    global isRunningDetection
+    isRunningDetection = True
+
+def pauseDetection(message = None):
+    global isRunningDetection
+    isRunningDetection = False
+
     
 isRaspberryPi = "linux" in _platform
 if(isRaspberryPi):
@@ -36,9 +47,11 @@ else:
 arduinoCommands = [["info", "s", ArduinoCommands.info],
             ["error", "s", ArduinoCommands.error],
             ["mpuData", "fff", ArduinoCommands.mpuData],
-            ["targetData", "fffff"]]
+            ["targetData", "fffff"],
+            ["startDetection", "", startDetection],
+            ["pauseDetection", "", pauseDetection]]
 
-bluetoothHandlers = {"start" : start, "stop" : stop}
+bluetoothHandlers = {"start" : start, "stop" : stop, "startDetection" : startDetection, "pauseDetection" : pauseDetection}
 
 arduino = ArduinoCom(arduinoPort, 9600, arduinoCommands)
 bluetooth = BluetoothCom(bluetoothPort, 9600, 0.1, bluetoothHandlers)
@@ -83,17 +96,18 @@ def loop():
         print(u'Não foi possível recuperar um frame da câmera')
         return None
 
-    #objects = detection.detect(frame, (5, 50, 50), (65, 255, 255))
-    objects = detection.detect(frame, (0, 0, 0), (360, 255, 255))
-    detected = len(objects) > 0
-    if detected:
-        (x, y, w, h) = objects[0]
-        objCenterX = x + (w / 2.0)
+    if(isRunningDetection):
+        #objects = detection.detect(frame, (5, 50, 50), (65, 255, 255))
+        objects = detection.detect(frame, (0, 0, 0), (360, 255, 255))
+        detected = len(objects) > 0
+        if detected:
+            (x, y, w, h) = objects[0]
+            objCenterX = x + (w / 2.0)
 
-        # Faz uma interpolação para calcular a direção
-        # do centro do objeto relativo ao centro da tela
-        direction = (objCenterX - 0) * (1 - (-1)) / (video.width - 0) + (-1)
-        arduino.send("targetData", direction, x, y, w, h)
+            # Faz uma interpolação para calcular a direção
+            # do centro do objeto relativo ao centro da tela
+            direction = (objCenterX - 0) * (1 - (-1)) / (video.width - 0) + (-1)
+            arduino.send("targetData", direction, x, y, w, h)
     window.update(frame)
     
 setup()
--- pi/videoStream/WebcamVideoStream.py
+++ pi/videoStream/WebcamVideoStream.py
@@ -3,36 +3,35 @@ from threading import Thread
 import cv2
 
 class WebcamVideoStream:
-	def __init__(self, src=0):
-		# initialize the video camera stream and read the first frame
-		# from the stream
-		self.stream = cv2.VideoCapture(src)
-		(self.grabbed, self.frame) = self.stream.read()
+    def __init__(self, src=0):
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+        self.src = src
+    def start(self):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(self.src)
+        (self.grabbed, self.frame) = self.stream.read()
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
  
-		# initialize the variable used to indicate if the thread should
-		# be stopped
-		self.stopped = False
-
-	def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                self.stream.release();
+                return
  
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		while True:
-			# if the thread indicator variable is set, stop the thread
-			if self.stopped:
-				self.stream.release();
-				return
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+            
+    def read(self):
+        # return the frame most recently read
+        return self.frame
  
-			# otherwise, read the next frame from the stream
-			(self.grabbed, self.frame) = self.stream.read()
- 
-	def read(self):
-		# return the frame most recently read
-		return self.frame
- 
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
