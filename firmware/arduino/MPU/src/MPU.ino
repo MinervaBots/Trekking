65021a21fdcae931b192f12f2c490dab1d3305c2
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2018-04-01 20:23:28 -0300

    Código de exemplo da SparkFun com um fusão dos 3 sensores

--- firmware/arduino/MPU/src/MPU.ino
+++ firmware/arduino/MPU/src/MPU.ino
@@ -1,79 +1,183 @@
-#include <SparkFunMPU9250-DMP.h>
-#include "Vector.h"
+#include <QuaternionFilters.h>
+#include <MPU9250.h>
 
-#define G 9.8
-#define BAUD_RATE 9600
-#define CALIBRATION_BUTTON 10
+// Pin definitions
+int myLed = 13; // Set up pin 13 led for toggling
 
-MPU9250_DMP imu;
-Vector3 acceleration, inertialAcceleration;
-Vector3 gyroscope, gyroBias;
-Vector3 compass, compassBias;
+#define SerialDebug true
+MPU9250 imu;
 
-void setupMPU();
 
 void setup()
 {
-  Serial.begin(BAUD_RATE);
+  Serial.begin(38400);
+  pinMode(myLed, OUTPUT);
+  digitalWrite(myLed, HIGH);
+}
+
+void setupIMU()
+{
   imu.begin();
-  setupMPU();
+
+  if (imu.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250) == 0x71) // WHO_AM_I should always be 0x71
+  {
+    Serial.println(F("MPU9250 is online..."));
+
+    // Start by performing self test and reporting values
+    imu.MPU9250SelfTest(imu.selfTest);
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    imu.calibrateMPU9250(imu.gyroBias, imu.accelBias);
+
+    imu.initMPU9250();
+    // Initialize device for active mode read of acclerometer, gyroscope, and
+    // temperature
+    Serial.println("MPU9250 initialized for active data mode....");
+
+    // Read the WHO_AM_I register of the magnetometer, this is a good test of
+    // communication
+    byte d = imu.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
+    Serial.print("AK8963 ");
+    Serial.print("I AM 0x");
+    Serial.print(d, HEX);
+    Serial.print(" I should be 0x");
+    Serial.println(0x48, HEX);
+
+    if (d != 0x48)
+    {
+      // Communication failed, stop here
+      Serial.println(F("Communication failed, abort!"));
+      Serial.flush();
+      abort();
+    }
+
+    // Get magnetometer calibration from AK8963 ROM
+    imu.initAK8963(imu.factoryMagCalibration);
+    // Initialize device for active mode read of magnetometer
+    Serial.println("AK8963 initialized for active data mode....");
+
+    // Get sensor resolutions, only need to do this once
+    imu.getAres();
+    imu.getGres();
+    imu.getMres();
+
+    // The next call delays for 4 seconds, and then records about 15 seconds of
+    // data to calculate bias and scale.
+    imu.magCalMPU9250(imu.magBias, imu.magScale);
+    Serial.println("AK8963 mag biases (mG)");
+    Serial.println(imu.magBias[0]);
+    Serial.println(imu.magBias[1]);
+    Serial.println(imu.magBias[2]);
+
+    Serial.println("AK8963 mag scale (mG)");
+    Serial.println(imu.magScale[0]);
+    Serial.println(imu.magScale[1]);
+    Serial.println(imu.magScale[2]);
+    delay(2000); // Add delay to see results before serial spew of data
+
+    if (SerialDebug)
+    {
+      Serial.println("Magnetometer:");
+      Serial.print("X-Axis sensitivity adjustment value ");
+      Serial.println(imu.factoryMagCalibration[0], 2);
+      Serial.print("Y-Axis sensitivity adjustment value ");
+      Serial.println(imu.factoryMagCalibration[1], 2);
+      Serial.print("Z-Axis sensitivity adjustment value ");
+      Serial.println(imu.factoryMagCalibration[2], 2);
+    }
+  } // if (c == 0x71)
+  else
+  {
+    Serial.print("Could not connect to MPU9250: 0x");
+
+    // Communication failed, stop here
+    Serial.println(F("Communication failed, abort!"));
+    Serial.flush();
+    abort();
+  }
 }
 
 void loop()
 {
-  if (imu.dataReady())
+  if (imu.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
   {
-    readMPU();
-  }
-}
+    imu.readAccelData(imu.accelCount); // Read the x/y/z adc values
 
-void setupMPU()
-{
-  if (imu.begin() != INV_SUCCESS)
+    // Now we'll calculate the accleration value into actual g's
+    // This depends on scale being set
+    imu.ax = (float)imu.accelCount[0] * imu.aRes; // - imu.accelBias[0];
+    imu.ay = (float)imu.accelCount[1] * imu.aRes; // - imu.accelBias[1];
+    imu.az = (float)imu.accelCount[2] * imu.aRes; // - imu.accelBias[2];
+
+    imu.readGyroData(imu.gyroCount); // Read the x/y/z adc values
+
+    // Calculate the gyro value into actual degrees per second
+    // This depends on scale being set
+    imu.gx = (float)imu.gyroCount[0] * imu.gRes;
+    imu.gy = (float)imu.gyroCount[1] * imu.gRes;
+    imu.gz = (float)imu.gyroCount[2] * imu.gRes;
+
+    imu.readMagData(imu.magCount); // Read the x/y/z adc values
+
+    // Calculate the magnetometer values in milliGauss
+    // Include factory calibration per data sheet and user environmental
+    // corrections
+    // Get actual magnetometer value, this depends on scale being set
+    imu.mx = (float)imu.magCount[0] * imu.mRes * imu.factoryMagCalibration[0] - imu.magBias[0];
+    imu.my = (float)imu.magCount[1] * imu.mRes * imu.factoryMagCalibration[1] - imu.magBias[1];
+    imu.mz = (float)imu.magCount[2] * imu.mRes * imu.factoryMagCalibration[2] - imu.magBias[2];
+  } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+
+  // Must be called before updating quaternions!
+  imu.updateTime();
+
+  // Sensors x (y)-axis of the accelerometer is aligned with the y (x)-axis of
+  // the magnetometer; the magnetometer z-axis (+ down) is opposite to z-axis
+  // (+ up) of accelerometer and gyro! We have to make some allowance for this
+  // orientationmismatch in feeding the output to the quaternion filter. For the
+  // MPU-9250, we have chosen a magnetic rotation that keeps the sensor forward
+  // along the x-axis just like in the LSM9DS0 sensor. This rotation can be
+  // modified to allow any convenient orientation convention. This is ok by
+  // aircraft orientation standards! Pass gyro rate as rad/s
+  MahonyQuaternionUpdate(imu.ax, imu.ay, imu.az, imu.gx * DEG_TO_RAD,
+                         imu.gy * DEG_TO_RAD, imu.gz * DEG_TO_RAD, imu.my,
+                         imu.mx, imu.mz, imu.deltat);
+
+  // Serial print and/or display at 0.5 s rate independent of data rates
+  imu.delt_t = millis() - imu.count;
+
+  // update LCD once per half-second independent of read rate
+  if (imu.delt_t > 500)
   {
-    while (1)
-    {
-      Serial.println("Unable to communicate with MPU-9250");
-      Serial.println("Check connections, and try again.");
-      Serial.println();
-      delay(5000);
-    }
-  }
+    // Define output variables from updated quaternion---these are Tait-Bryan
+    // angles, commonly used in aircraft orientation. In this coordinate system,
+    // the positive z-axis is down toward Earth. Yaw is the angle between Sensor
+    // x-axis and Earth magnetic North (or true North if corrected for local
+    // declination, looking down on the sensor positive yaw is counterclockwise.
+    // Pitch is angle between sensor x-axis and Earth ground plane, toward the
+    // Earth is positive, up toward the sky is negative. Roll is angle between
+    // sensor y-axis and Earth ground plane, y-axis up is positive roll. These
+    // arise from the definition of the homogeneous rotation matrix constructed
+    // from quaternions. Tait-Bryan angles as well as Euler angles are
+    // non-commutative; that is, the get the correct orientation the rotations
+    // must be applied in the correct order which for this configuration is yaw,
+    // pitch, and then roll.
+    // For more see
+    // http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+    // which has additional links.
+    imu.yaw = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ() * *(getQ() + 3)), *getQ() * *getQ() + *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) - *(getQ() + 3) * *(getQ() + 3));
+    imu.pitch = -asin(2.0f * (*(getQ() + 1) * *(getQ() + 3) - *getQ() * *(getQ() + 2)));
+    imu.roll = atan2(2.0f * (*getQ() * *(getQ() + 1) + *(getQ() + 2) * *(getQ() + 3)), *getQ() * *getQ() - *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) + *(getQ() + 3) * *(getQ() + 3));
+    imu.pitch *= RAD_TO_DEG;
+    imu.yaw *= RAD_TO_DEG;
 
-  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
-  imu.setGyroFSR(2000); // Set gyro to 2000 dps
-  imu.setAccelFSR(4); // Set accel to +/-4g
-  imu.setLPF(5); // Set LPF corner frequency to 5Hz
-  imu.setCompassSampleRate(100); // Set mag rate to 100Hz
+    // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
+    // 	8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
+    // - http://www.ngdc.noaa.gov/geomag-web/#declination
+    imu.yaw -= 8.5;
+    imu.roll *= RAD_TO_DEG;
 
-  // Calcula valores de referência com o robô parado
-  imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
-  acceleration.X = imu.calcAccel(imu.ax);
-  acceleration.Y = imu.calcAccel(imu.ay);
-  acceleration.Z = imu.calcAccel(imu.az);
-  
-  acceleration.X = imu.calcGyro(imu.gx);
-  acceleration.Y = imu.calcGyro(imu.gy);
-  acceleration.Z = imu.calcGyro(imu.gz);
-  
-  acceleration.X = imu.calcMag(imu.mx);
-  acceleration.Y = imu.calcMag(imu.my);
-  acceleration.Z = imu.calcMag(imu.mz);
-}
-
-void readMPU()
-{
-  imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
-  
-  acceleration.X = imu.calcAccel(imu.ax);
-  acceleration.Y = imu.calcAccel(imu.ay);
-  acceleration.Z = imu.calcAccel(imu.az);
-  
-  acceleration.X = imu.calcGyro(imu.gx);
-  acceleration.Y = imu.calcGyro(imu.gy);
-  acceleration.Z = imu.calcGyro(imu.gz);
-  
-  acceleration.X = imu.calcMag(imu.mx);
-  acceleration.Y = imu.calcMag(imu.my);
-  acceleration.Z = imu.calcMag(imu.mz);
-}
+    imu.count = millis();
+    imu.sumCount = 0;
+    imu.sum = 0;
+  } // if (imu.delt_t > 500)
+} // if (AHRS)
--- firmware/arduino/MPU/src/Vector.h
+++ firmware/arduino/MPU/src/Vector.h
@@ -1,14 +1,53 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-struct Vector2
+struct Vector3
 {
-  float X, Y;
-};
+  float X, Y, Z;
 
-struct Vector3 : Vector2
-{
-  float Z;
-}
+  float module(bool useX, bool useY, bool useZ)
+  {
+    float result = 0;
+    
+    if(useX)
+    {
+      result += pow(X, 2);
+    }
+    if(useY)
+    {
+      result += pow(Y, 2);
+    }
+    if(useZ)
+    {
+      result += pow(Z, 2);
+    }
+    
+    return sqrt(result);
+  }
+
+  static Vector3 lerp(Vector3 vectorA, Vector3 vectorB, float alpha)
+  {
+    Vector3 resultVector;
+    resultVector.X = ((1 - alpha) * vectorA.X + alpha * vectorB.X);
+    resultVector.Y = ((1 - alpha) * vectorA.Y + alpha * vectorB.Y);
+    resultVector.Z = ((1 - alpha) * vectorA.Z + alpha * vectorB.Z);
+    return resultVector;
+  }
+
+  Vector3& operator+=(const Vector3& rhs)
+  {
+    X += rhs.X;
+    Y += rhs.Y;
+    Z += rhs.Z;
+    return *this;
+  }
+  Vector3& operator-=(const Vector3& rhs)
+  {
+    X -= rhs.X;
+    Y -= rhs.Y;
+    Z -= rhs.Z;
+    return *this;
+  }
+};
 
 #endif
