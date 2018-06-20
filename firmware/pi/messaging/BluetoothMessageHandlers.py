3b8fb13a55caf16d266002964951b3b29d063725
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2018-06-20 11:12:04 -0300

    Corrige problemas nas threads de comunicação entre os dispositivos

--- firmware/pi/messaging/BluetoothMessageHandlers.py
+++ firmware/pi/messaging/BluetoothMessageHandlers.py
@@ -11,67 +11,67 @@ from SystemInfo import SystemInfo
 from .MessagingThread import MessagingThread
 
 class BluetoothMessageCodes(IntEnum):
-    START_SYSTEM = 100
-    STOP_SYSTEM = auto()
-    SET_TRACKING_METHOD = auto()
-    GET_FPS = auto()
-    GET_TEMPERATURE = auto()
+  START_SYSTEM = 100
+  STOP_SYSTEM = auto()
+  SET_TRACKING_METHOD = auto()
+  GET_FPS = auto()
+  GET_TEMPERATURE = auto()
 
 	
 class BluetoothMessageHandlersModule(dic.container.Module):
-    def load(self, builder):
-        builder.register_class(StartSystemHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
-        builder.register_class(StopSystemHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
-        builder.register_class(SetTrackingMethodHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
-        builder.register_class(GetFPSHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
-        builder.register_class(GetTemperatureHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
+  def load(self, builder):
+    builder.register_class(StartSystemHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
+    builder.register_class(StopSystemHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
+    builder.register_class(SetTrackingMethodHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
+    builder.register_class(GetFPSHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
+    builder.register_class(GetTemperatureHandler, register_as = [BluetoothMessageHandler], component_scope=dic.scope.SingleInstance)
 
 		
 class BluetoothMessageHandler(MessageHandler, ABC):
-    def __init__(self):
-        self.opCode = 0
-    
-    @abstractmethod
-    def handle(self, sender : MessagingThread, message : List):
-        raise NotImplementedError
+  def __init__(self):
+    self.opCode = 0
+  
+  @abstractmethod
+  def handle(self, sender : MessagingThread, message : List):
+    raise NotImplementedError
 
-    
+  
 class StartSystemHandler(BluetoothMessageHandler):
-    def __init__(self, systemInfo : SystemInfo):
-        self.opCode = BluetoothMessageCodes.START_SYSTEM
-        self.__systemInfo = systemInfo
-        
-    def handle(self, sender : MessagingThread, message : List):
-        self.__systemInfo.isRunning = True
-        
+  def __init__(self, systemInfo : SystemInfo):
+    self.opCode = BluetoothMessageCodes.START_SYSTEM
+    self.__systemInfo = systemInfo
+    
+  def handle(self, sender : MessagingThread, message : List):
+    self.__systemInfo.isRunning = True
+    
 class StopSystemHandler(BluetoothMessageHandler):
-    def __init__(self, systemInfo : SystemInfo):
-        self.opCode = BluetoothMessageCodes.STOP_SYSTEM
-        self.__systemInfo = systemInfo
-        
-    def handle(self, sender : MessagingThread, message : List):
-        self.__systemInfo.isRunning = False
-        
+  def __init__(self, systemInfo : SystemInfo):
+    self.opCode = BluetoothMessageCodes.STOP_SYSTEM
+    self.__systemInfo = systemInfo
+    
+  def handle(self, sender : MessagingThread, message : List):
+    self.__systemInfo.isRunning = False
+    
 class SetTrackingMethodHandler(BluetoothMessageHandler):
-    def __init__(self, tracker : Tracker):
-        self.opCode = BluetoothMessageCodes.SET_TRACKING_METHOD
-        self.__tracker = tracker
+  def __init__(self, tracker : Tracker):
+    self.opCode = BluetoothMessageCodes.SET_TRACKING_METHOD
+    self.__tracker = tracker
+  
+  def handle(self, sender : MessagingThread, message : List):
+    self.__tracker.setTrackingMethod(message[1])
     
-    def handle(self, sender : MessagingThread, message : List):
-        self.__tracker.setTrackingMethod(message[0])
-        
 class GetFPSHandler(BluetoothMessageHandler):
-    def __init__(self, fps : FPS):
-        self.opCode = BluetoothMessageCodes.GET_FPS
-        self.__fps = fps
+  def __init__(self, fps : FPS):
+    self.opCode = BluetoothMessageCodes.GET_FPS
+    self.__fps = fps
+  
+  def handle(self, sender : MessagingThread, message : List):
+    sender.send(self.opCode, "FPS: {:.2f}".format(self.__fps.lastFps()))
     
-    def handle(self, sender : MessagingThread, message : List):
-        sender.send(self.opCode, "FPS: {:.2f}".format(self.__fps.lastFps()))
-        
 class GetTemperatureHandler(BluetoothMessageHandler):
-    def __init__(self, temp : TemperatureControl):
-        self.opCode = BluetoothMessageCodes.GET_TEMPERATURE
-        self.__temp = temp
-    
-    def handle(self, sender : MessagingThread, message : List):
-        sender.send(self.opCode, "Temp: {:.2f} 'C".format(self.__temp.update()))
+  def __init__(self, temp : TemperatureControl):
+    self.opCode = BluetoothMessageCodes.GET_TEMPERATURE
+    self.__temp = temp
+  
+  def handle(self, sender : MessagingThread, message : List):
+    sender.send(self.opCode, "Temp: {:.2f} 'C".format(self.__temp.update()))
