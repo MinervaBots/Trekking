109c6308b384583e4dfd3c7b85041bab3e202b5b
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2018-06-21 11:43:11 -0300

    Corrige calculo de direção do objeto detectado em relação a câmera

--- firmware/pi/tracking/Tracker.py
+++ firmware/pi/tracking/Tracker.py
@@ -101,9 +101,8 @@ class Tracker:
     # do centro do objeto relativo ao centro da tela
     # Mapeia a posição em pixels na tela para uma direção entre -1 e 1
     def __rectToDirection(self, rect):
-        objCenterX = rect[0] + (rect[2] / 2.0)
-        #direction = float(interp1d([0, self.resolution[0]], [-1, 1])(objCenterX))
-        return 0
+        objCenterX = (((rect[0] + (rect[2] / 2.0)) / self.resolution[0]) - 0.5) * 2
+        return objCenterX
 
     @staticmethod
     def rectToPoints(rect):
